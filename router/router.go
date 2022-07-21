da733aa33bb2c9adb22081b80eae55626e4ec736
albertopformoso <albertopformoso@gmail.com>
2022-07-21 11:17:57 -0500

    base path

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"GoConcurrency-Bootcamp-2022/controllers"
 	"GoConcurrency-Bootcamp-2022/repositories"
 	"GoConcurrency-Bootcamp-2022/use_cases"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,6 +24,9 @@ func Init() *gin.Engine {
 	r.POST("/api/provide", ctrl.FillCSV)
 	r.PUT("/api/refresh-cache", ctrl.RefreshCache)
 	r.GET("/api/pokemons", ctrl.GetPokemons)
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Wizeline Go Concurrency Bootcamp 2022")
+	})
 
 	return r
 }
